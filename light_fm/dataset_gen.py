aa2948e25caeb7f44ad488b521a6a484c8fe6dad
ABICHAL GHOSH <abichalghosh@gmail.com>
2023-11-12 16:55:27 -0700

    added train test split for movielens 1m

--- light_fm/dataset_gen.py
+++ light_fm/dataset_gen.py
@@ -1,26 +1,55 @@
-def preprocess_data(data_dir):
-	# EDIT
+import pandas as pd
+import os
+import csv
+import requests
+import json
+from itertools import islice
+
+import lightfm
+from lightfm.data import Dataset
+
+
+# Adding the base dir
+base_path = "./.."
+
+# Setting constant seed for reproducibility
+SEED = 42
+
+def split_data(data_dir):
+	print("===========================================")
+	print("Splitting the Data")
+	print("===========================================")
 	data_dir = base_path+"/"+data_dir
-	print("===========================================")
-	print("Preprocessing the Data")
-	print("===========================================")
-	print("The files present in the data directory are given as-")
+
+	print("Data files are:")
 	print(os.listdir(data_dir))
 
-	movie_columns = ['movie_id', 'title', 'genres']
-	movies = pd.read_table(data_dir+"/movies.dat", sep = "::", header = None, names = movie_columns, encoding = "latin-1")
+	print("Loading and splitting the ratings.csv file")
 
+	# Load ratings.csv
 	rating_columns = ['user_id', 'movie_id', 'rating', 'timestamp']
 	ratings = pd.read_table(data_dir+"/ratings.dat", sep = "::", header = None, names = rating_columns, encoding = "latin-1")
 
-	user_columns = ['user_id', 'gender', 'age', 'occupation', 'zip']
-	users = pd.read_table(data_dir+"/users.dat", sep = "::", header = None, names = user_columns, encoding = "latin-1")
+	# Splitting the data
+	# Creating train data
+	ratings_train = ratings.sample(frac=0.8, random_state = SEED)
 
-	# Preprocessing the ratings to create 
-	print(ratings)
+	# Dropping the train from ratings to get test
+	ratings_test = ratings.drop(ratings_train.index)
 
-	# Creating the dataset object
-	dataset = Dataset()
+	# Checking shapes of train and test
+	print("Train test split completed")
+	print("___________________________")
+	print("train shape:")
+	print(ratings_train.shape)
+	print("___________________________")
+	print("test shape:")
+	print(ratings_test.shape)
+	print("___________________________")
 
-	
-	return " "
+	ratings_train.to_csv(data_dir+"/random_split/ratings_train.csv")
+	ratings_test.to_csv(data_dir+"/random_split/ratings_test.csv")
+
+
+data_dir = "datasets/ml-1m"
+split_data(data_dir)

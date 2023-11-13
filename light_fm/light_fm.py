9db6d0fe95fe8a200bd76bddca68776ec86f192a
husainasad <asad1999@gmail.com>
2023-11-13 06:07:47 -0700

    implemented fetch and split datasets functionality

--- light_fm/light_fm.py
+++ light_fm/light_fm.py
@@ -4,15 +4,17 @@ import pandas as pd
 import pickle
 
 from lightfm import LightFM
-from lightfm.datasets import fetch_movielens
+from lightfm.cross_validation import random_train_test_split
 from lightfm.evaluation import precision_at_k
 from lightfm.evaluation import auc_score
 from lightfm.evaluation import recall_at_k
 from lightfm.evaluation import reciprocal_rank
+from spotlight.datasets import movielens
+
 
 def create_model(train_data, loss_function, pickle_file):
   model = LightFM(learning_rate=0.05, loss=loss_function)
-  model.fit(train_data, epochs=10, num_threads=4)
+  %time model.fit(train_data, epochs=10, num_threads=4, verbose=True)
   pickle.dump(model, open(pickle_file, 'wb'))
 
 def evaluate_model(train_data, test_data, pickle_file):
@@ -37,29 +39,20 @@ def evaluate_model(train_data, test_data, pickle_file):
 def create_embeddings():
   pass
 
-def build_recommender(name='official'):
+def build_recommender(name):
 
-  if name=='official':
-    data = fetch_movielens()
-    train = data['train']
-    test = data['test']
-  else:
-    data_path = './../datasets/'+name
-    train_path = data_path+'/random_split/ratings_train.csv'
-    test_path = data_path+'/random_split/ratings_train.csv'
-    train, test = create_embeddings(data_path, train_path, test_path)
+  data = movielens.get_movielens_dataset(variant=name).tocoo()
+  train, test = random_train_test_split(data)
 
-  loss_types= ['bpr', 'warp']
+  # loss_types= ['bpr', 'warp']
+  loss_types = ['warp']
 
   for loss in loss_types:
     pickle_name = name+'_'+loss+'_random.pkl'
     create_model(train, loss, pickle_name)
     evaluate_model(train, test, pickle_name)
 
+datasets = ['100K', '1M', '10M', '20M']
 
-build_recommender()
-
-# datasets = ['ml-100k', 'ml-1m', 'ml-10m', 'ml-20m']
-
-# for dataset in datasets:
-#   build_recommender(dataset)
+for dataset in datasets:
+  build_recommender(dataset)

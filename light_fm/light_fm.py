4439f65f747d6779691ab50d0a9a62415741062a
husainasad <asad1999@gmail.com>
2023-11-14 04:09:43 -0700

    updated lightfm to use item features with tuned hyperparameters (1M)

--- light_fm/light_fm.py
+++ light_fm/light_fm.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pandas as pd
 import pickle
+import numpy as np
 
 from lightfm import LightFM
 from lightfm.cross_validation import random_train_test_split
@@ -9,50 +10,52 @@ from lightfm.evaluation import precision_at_k
 from lightfm.evaluation import auc_score
 from lightfm.evaluation import recall_at_k
 from lightfm.evaluation import reciprocal_rank
-from spotlight.datasets import movielens
+from scipy import sparse
 
 
-def create_model(train_data, loss_function, pickle_file):
-  model = LightFM(learning_rate=0.05, loss=loss_function)
-  model.fit(train_data, epochs=10, num_threads=6, verbose=True)
-  pickle.dump(model, open(pickle_file, 'wb'))
+SEED = 42
+epochs = 34
+learning_rate = 0.221
 
-def evaluate_model(train_data, test_data, pickle_file):
-  model = pickle.load(open(pickle_file, 'rb'))
-  train_precision = precision_at_k(model, train_data).mean()
-  test_precision = precision_at_k(model, test_data).mean()
+def create_model(train_data, loss_function, item_features, pickle_file):
+	model = LightFM(learning_rate=learning_rate, loss=loss_function, random_state=np.random.RandomState(SEED))
+	model.fit(train_data, item_features=item_features, epochs=epochs, verbose=True)
+	pickle.dump(model, open(pickle_file, 'wb'))
 
-  train_auc = auc_score(model, train_data).mean()
-  test_auc = auc_score(model, test_data).mean()
+def evaluate_model(train_data, test_data, item_features, pickle_file):
+	model = pickle.load(open(pickle_file, 'rb'))
+	train_precision = precision_at_k(model, train_data, item_features=item_features).mean()
+	test_precision = precision_at_k(model, test_data, item_features=item_features).mean()
 
-  train_recall = recall_at_k(model, train_data).mean()
-  test_recall = recall_at_k(model, test_data).mean()
+	train_auc = auc_score(model, train_data, item_features=item_features).mean()
+	test_auc = auc_score(model, test_data, item_features=item_features).mean()
 
-  train_rank = reciprocal_rank(model, train_data).mean()
-  test_rank = reciprocal_rank(model, test_data).mean()
+	train_recall = recall_at_k(model, train_data, item_features=item_features).mean()
+	test_recall = recall_at_k(model, test_data, item_features=item_features).mean()
 
-  print('Precision: train %.2f, test %.2f.' % (train_precision, test_precision))
-  print('AUC: train %.2f, test %.2f.' % (train_auc, test_auc))
-  print('Recall: train %.2f, test %.2f.' % (train_recall, test_recall))
-  print('Rank: train %.2f, test %.2f.' % (train_rank, test_rank))
+	train_rank = reciprocal_rank(model, train_data, item_features=item_features).mean()
+	test_rank = reciprocal_rank(model, test_data, item_features=item_features).mean()
 
-def create_features():
-  pass
+	print('Precision: train %.2f, test %.2f.' % (train_precision, test_precision))
+	print('AUC: train %.2f, test %.2f.' % (train_auc, test_auc))
+	print('Recall: train %.2f, test %.2f.' % (train_recall, test_recall))
+	print('Rank: train %.2f, test %.2f.' % (train_rank, test_rank))
 
 def build_recommender(name):
+	data = sparse.load_npz("interactions.npz")
+	item_features = sparse.load_npz("item_features.npz")
+	train, test = random_train_test_split(data, random_state=np.random.RandomState(SEED))
 
-  data = movielens.get_movielens_dataset(variant=name).tocoo()
-  train, test = random_train_test_split(data)
+	# loss_types= ['bpr', 'warp']
+	loss_types = ['warp']
 
-  # loss_types= ['bpr', 'warp']
-  loss_types = ['warp']
+	for loss in loss_types:
+		pickle_name = '{0}_{1}_random.pkl'.format(name, loss)
+		create_model(train, loss, item_features, pickle_name)
+		evaluate_model(train, test, item_features, pickle_name)
 
-  for loss in loss_types:
-    pickle_name = name+'_'+loss+'_random.pkl'
-    create_model(train, loss, pickle_name)
-    evaluate_model(train, test, pickle_name)
+# datasets = ['100K', '1M', '10M', '20M']
 
-datasets = ['100K', '1M', '10M', '20M']
-
-for dataset in datasets:
-  build_recommender(dataset)
+# for dataset in datasets:
+# 	build_recommender(dataset)
+build_recommender('1M')
